978a75d62e4b4b3d21d5c81e0a00e19ccf9d9c8b
Claire <claire.irawan@wustl.edu>
2020-09-28 15:19:04 -0500

    style

--- main.php
+++ main.php
@@ -8,6 +8,7 @@ $username=$_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="fileshare.css" type="text/css" rel="stylesheet" />
     <title>File Share</title>
 </head>
 <body>
