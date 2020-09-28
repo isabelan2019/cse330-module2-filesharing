f4ca3a9549bd82770e16fd07b7a94c752230a070
Claire <claire.irawan@wustl.edu>
2020-09-28 13:37:22 -0500

    delete works

--- delete.php
+++ delete.php
@@ -3,6 +3,25 @@
 session_start();
 
 $filename = $_POST['file'];
+$username = $_SESSION['username'];
+
+if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
+	echo "Invalid filename";
+	exit;
+}
+
+$username = $_SESSION['username'];
+if( !preg_match('/^[\w_\-]+$/', $username) ){
+	echo "Invalid username";
+	exit;
+}
+
 $full_path = sprintf("/srv/fileshare_module/uploads/%s/%s", $username, $filename);
 
-?>
+if (unlink($full_path)) {
+    echo "deleted";
+} else {
+    echo "not deleted";
+}
+
+?> 
