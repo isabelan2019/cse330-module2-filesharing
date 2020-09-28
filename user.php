855a2aaf5ff6b5c40b86a851d1648e4ef6b07f59
Isabel An <isabel.an@wustl.edu>
2020-09-27 20:00:48 -0500

    login page works (no session var)

--- user.php
+++ user.php
@@ -2,21 +2,43 @@
 session_start();
 
 $username = (string) $_GET["username"];
-$userarray = file("/srv/users.txt");
+$validUser = FALSE;
+
+//read file line by line 
+$h = fopen("/srv/users.txt", "r");
+
+while( !feof($h) ){
+    if (trim(fgets($h))=="$username"){
+    //    header("Location:main.html");
+        $validUser = TRUE;
+        break;
+    } 
+}
+
+fclose($h);
+
+if($validUser){
+    header("Location:main.html");
+}
+else{
+    header("Location:login.html");
+}
+
+//$userarray = file("/srv/users.txt");
 // users: username, user.name, test.user
 
-print_r($userarray);
+//print_r($userarray);
 //file_get_contents("/srv/users.txt","r");
 
-for ($i=0; $i<count($userarray); $i++) {
+//for ($i=0; $i<count($userarray); $i++) {
     
-    if ( $userarray[$i] === $username ) {
-        echo $userarray[$i];
-        echo "yay";
-    } else {
-        echo "boo";
-    }
-}
+//    if ( $userarray[$i] === $username ) {
+//        echo $userarray[$i];
+//        echo "yay";
+//    } else {
+//        echo "boo";
+//    }
+//}
 
 // if ( in_array($username, $userarray) ) {
 //     header( "Location:main.html"); //should be specific to user, i.e. main.html/ .. 
