ad8575eee855f786eed492c05d1278abbb0fefc1
Claire <claire.irawan@wustl.edu>
2020-09-28 22:40:53 -0500

    add user folder

--- adduser.php
+++ adduser.php
@@ -8,12 +8,18 @@ if( !preg_match('/^[\w_\-]+$/', $_SESSION['newUser']) ){
 	echo htmlentities("Invalid username. Try a username with no special characters.");
 	exit;
 } else {
-$h = "/srv/fileshare_module/users.txt";
-$newUser = $_SESSION['newUser'];
-file_put_contents($h, $newUser, FILE_APPEND | LOCK_EX);
-$newline = "\n";
-file_put_contents($h, $newline, FILE_APPEND | LOCK_EX);
-echo htmlentities("done! go back and you will be able to login");
+	// adds user to users.txt 
+	$h = "/srv/fileshare_module/users.txt";
+	$newUser = $_SESSION['newUser']; 
+	file_put_contents($h, $newUser, FILE_APPEND | LOCK_EX);
+	$newline = "\n";
+	file_put_contents($h, $newline, FILE_APPEND | LOCK_EX);
+
+	//adds user folder
+	$uploadFolder = sprintf("/srv/fileshare_module/uploads/%s", $newUser);
+	mkdir($uploadFolder, 0777, true);
+	// chmod($uploadFolder, 0777);
+	echo htmlentities("done! go back and you will be able to login");
 }
 
 ?>
